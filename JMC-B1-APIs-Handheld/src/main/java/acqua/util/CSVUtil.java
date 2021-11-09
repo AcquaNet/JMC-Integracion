27184c4285dfee20b6d9b1362f183d698bd45a8c
Santiago Repetto <srepetto@acqua.net.ar>
2021-11-09 10:23:11 -0300

    Include csv file creator java class

---
+++ JMC-B1-APIs-Handheld/src/main/java/acqua/util/CSVUtil.java
@@ -0,0 +1,46 @@
+package acqua.util;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder;
+
+import org.apache.log4j.Logger;
+
+public class CSVUtil {
+	private static final Logger LOG = Logger.getLogger("jmc_java.log");
+	
+	public static void csvWriter(String csvData, String path, String fileName) {
+		
+		try {
+			JsonNode jsonTree = new ObjectMapper().readTree(csvData);
+			
+			Builder csvSchemaBuilder = CsvSchema.builder();
+			JsonNode firstObject = jsonTree.elements().next();
+			firstObject.fieldNames().forEachRemaining(fieldName -> {csvSchemaBuilder.addColumn(fieldName);} );
+			CsvSchema csvSchema = csvSchemaBuilder.build()
+									.withHeader()
+									.withColumnSeparator(';')
+									.withNullValue("")
+									.withoutQuoteChar();
+			
+			CsvMapper csvMapper = new CsvMapper();
+			String fileTimestamp = new SimpleDateFormat("_yyyyMMdd_HHmmssSSS").format(new Date());
+			
+			
+			csvMapper.writerFor(JsonNode.class).with(csvSchema).writeValue(new File(path + "/" + fileName + fileTimestamp + ".csv"), jsonTree);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Failed: " + e.getMessage());
+			LOG.error("CSVWriter Failed: " + e.getMessage());
+		} 
+	}
+
+}
